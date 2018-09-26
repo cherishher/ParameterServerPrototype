f66b7a6ec8afa15f45b3c0d3feaf3853b611924a
李喆 <lizhe@lizhedeMacBook-Pro.local>
2018-09-26 20:34:53 +0800

    slice

--- base/range_partition_manager.hpp
+++ base/range_partition_manager.hpp
@@ -46,27 +46,62 @@ class RangePartitionManager : public AbstractPartitionManager {
   			}
   		}
 
+  		// insert values to slice
   		if(!existTag){
+  			// if do not find it in slice
   			Keys tempkey;
   			tempkey.push_back(keys[i]);
   			std::pair<int, Keys> temppair(threadid, tempkey);
   			sliced->push_back(temppair);
   		} else {
+  			// if the server already in slice
   			(*sliced)[sliceid].second.push_back(keys[i]);
   		}
   	}
-  	// std::cerr << "=============" << endl;
-  	// for(int i = 0; i < sliced.size(); i++){
-  	// 	cerr << i << endl;
-  	// 	cerr << (*sliced)[i].first << endl;
-  	// 	for(int j = 0; j < (*sliced)[i].second.size(); j++){
-  	// 		cerr << (*sliced)[i].second[i] << " ";
-  	// 	}
-  	// }
+
   }
 
   void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const override {
- 
+  	bool existTag = false;
+  	int sliceid;
+  	int threadid;
+
+  	int totalSize = kvs.first.size();
+  	for(int i = 0; i < totalSize; i++){
+
+  		existTag = false;
+
+  		// find thread id
+  		for(int i1 = 0; i1 < rangesAndThreadId.size(); i1++){
+	  		if(kvs.first[i]>= rangesAndThreadId[i1].first.begin() and kvs.first[i] <rangesAndThreadId[i1].first.end()){
+	  			threadid = rangesAndThreadId[i1].second;
+	  		}
+  		}
+
+  		// check if exist in sliced
+  		for(int i2 = 0; i2 < sliced->size(); i2++){
+  			if(threadid == (*sliced)[i2].first){
+  				existTag = true;
+  				sliceid = i2;
+  			}
+  		}
+
+  		// insert values to slice
+  		if(!existTag){
+  			// if do not find it in slice
+  			Keys tempkey;
+  			tempkey.push_back(kvs.first[i]);
+  			third_party::SArray<double> tempvalue;
+  			tempvalue.push_back(kvs.second[i]);
+
+  			std::pair<int, KVPairs> temppair(threadid, KVPairs(tempkey,tempvalue));
+  			sliced->push_back(temppair);
+  		} else {
+  			// if the server already in slice
+  			(*sliced)[sliceid].second.first.push_back(kvs.first[i]);
+  			(*sliced)[sliceid].second.second.push_back(kvs.second[i]);
+  		}
+  	}
   }
 
  private:
