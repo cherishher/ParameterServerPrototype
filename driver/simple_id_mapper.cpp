a29850a2948976bd881f5adfa7535c3655d41b7d
maxHH <490949611@qq.com>
2018-10-25 15:35:58 +0800

    compelete simple_id_mapper

--- driver/simple_id_mapper.cpp
+++ driver/simple_id_mapper.cpp
@@ -8,35 +8,80 @@
 namespace csci5570 {
 
 SimpleIdMapper::SimpleIdMapper(Node node, const std::vector<Node>& nodes) {
-  // TODO
+  node_ = node;
+  nodes_ = nodes;
 }
 
-uint32_t SimpleIdMapper::GetNodeIdForThread(uint32_t tid) {
-  // TODO
-}
+uint32_t SimpleIdMapper::GetNodeIdForThread(uint32_t tid) { return tid / kMaxThreadsPerNode; }
 
 void SimpleIdMapper::Init(int num_server_threads_per_node) {
-  // TODO
+  if (num_server_threads_per_node >= 1 && num_server_threads_per_node <= kMaxThreadsPerNode) {
+    for (auto node : nodes_) {
+      for (int i = 0; i < num_server_threads_per_node; i++) {
+        // node2server_.insert(std::make_pair(node.id,node.id*kMaxThreadsPerNode+i));
+        std::vector<uint32_t> serverThread;
+        serverThread.insert(serverThread.begin() + i, node.id * kMaxThreadsPerNode);
+        node2server_[node.id] = serverThread;
+      }
+      std::vector<uint32_t> workerHelperThread;
+      workerHelperThread.insert(workerHelperThread.begin(), kWorkerHelperThreadId + node.id * kMaxThreadsPerNode);
+      node2worker_helper_.insert(std::make_pair(node.id, workerHelperThread));
+
+      std::set<uint32_t> workerThreads;
+      workerThreads.insert(node_.id*kMaxThreadsPerNode+kWorkerHelperThreadId+1);
+      node2worker_.insert(std::make_pair(node_.id,workerThreads));
+    };
+  }
 }
 
 uint32_t SimpleIdMapper::AllocateWorkerThread(uint32_t node_id) {
-  // TODO
-}
-void SimpleIdMapper::DeallocateWorkerThread(uint32_t node_id, uint32_t tid) {
-  // TODO
+  uint32_t allocateId;
+  try {
+    if (node2worker_[node_id].size() >= 0 && node2worker_[node_id].size() < kMaxThreadsPerNode - kMaxBgThreadsPerNode) {
+      std::set<uint32_t> workerSet = node2worker_[node_id];
+      allocateId = node_id * (kMaxThreadsPerNode + kMaxBgThreadsPerNode) + node2worker_[node_id].size();
+      workerSet.insert(allocateId);
+      node2worker_[node_id] = workerSet;
+    } else if(node2worker_[node_id].size() == 0){
+      std::set<uint32_t> workerSet;
+      allocateId = node_id * (kMaxThreadsPerNode + kMaxBgThreadsPerNode);
+      workerSet.insert(allocateId);
+      node2worker_[node_id] = workerSet;
+    }else{
+      allocateId == -1;
+    }
+  } catch (const std::exception& e) {
+    allocateId = -1;
+  }
+  return allocateId;
 }
 
-std::vector<uint32_t> SimpleIdMapper::GetServerThreadsForId(uint32_t node_id) {
-  // TODO
+void SimpleIdMapper::DeallocateWorkerThread(uint32_t node_id, uint32_t tid) {
+  if (node2worker_[node_id].size() != 0) {
+    node2worker_[node_id].erase(tid);
+  }
 }
+
+std::vector<uint32_t> SimpleIdMapper::GetServerThreadsForId(uint32_t node_id) { return node2server_[node_id]; }
 std::vector<uint32_t> SimpleIdMapper::GetWorkerHelperThreadsForId(uint32_t node_id) {
-  // TODO
+  return node2worker_helper_[node_id];
 }
 std::vector<uint32_t> SimpleIdMapper::GetWorkerThreadsForId(uint32_t node_id) {
-  // TODO
+  std::vector<uint32_t> workerThreads;
+  for (auto worker : node2worker_[node_id]) {
+    workerThreads.push_back(worker);
+  }
+  return workerThreads;
 }
+
 std::vector<uint32_t> SimpleIdMapper::GetAllServerThreads() {
-  // TODO
+  std::vector<uint32_t> serverThreads;
+  for (auto node : nodes_) {
+    std::vector<uint32_t> currentServerThreads = this->GetServerThreadsForId(node.id);
+    serverThreads.insert(serverThreads.end(), currentServerThreads.begin(), currentServerThreads.end());
+  }
+
+  return serverThreads;
 }
 
 const uint32_t SimpleIdMapper::kMaxNodeId;
