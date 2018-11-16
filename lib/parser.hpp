db6ba1908c8dc80a28174be730f2169841865662
maxHH <490949611@qq.com>
2018-11-16 18:10:25 +0800

    parser debug finish, now test_hdfs_read.cpp can read from hdfs smoothly

--- lib/parser.hpp
+++ lib/parser.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "boost/utility/string_ref.hpp"
+#include "glog/logging.h"
 
 namespace csci5570 {
 namespace lib {
@@ -18,21 +19,23 @@ class Parser {
     // hints: you may use boost::tokenizer, std::strtok_r, std::stringstream or any method you like
     // so far we tried all the tree and found std::strtok_r is fastest :)
     Sample sample;
+    char* pos;
 
-    char* dataptr(new char(line.size()));
-    strncpy(dataptr, line.data(), line.size());
-    char* token = strtok_r(dataptr, "\t:", &dataptr);
+    std::unique_ptr<char> dataptr(new char[line.size() + 1]);
+    strncpy(dataptr.get(), line.data(), line.size());
+    dataptr.get()[line.size()] = '\0';
+    char* token = strtok_r(dataptr.get(), " \t:", &pos);
 
     int i = -1;
     int key;
     float val;
     int feature_num = 0;
-    while (feature_num < n_features) {
+    while (true) {
       if (i == 0) {
         key = std::atoi(token) - 1;
-        if(key > feature_num){
-          for(int j=feature_num;j<key;j++){
-             sample.x_.push_back(std::make_pair(feature_num, 0));
+        if (key > feature_num) {
+          for (int j = feature_num; j < key; j++) {
+            sample.x_.push_back(std::make_pair(j, 0));
           }
           feature_num = key;
         }
@@ -47,8 +50,15 @@ class Parser {
         i = 0;
       }
       // Next key/value pair
-      token = strtok_r(NULL, "\t:", &dataptr);
+      token = strtok_r(NULL, " \t:", &pos);
+      if(token == NULL){
+        for(int j = feature_num; j < n_features ; j++){
+          sample.x_.push_back(std::make_pair(j, 0));          
+        }
+        break;
+      }
     }
+
     return sample;
   }
 
