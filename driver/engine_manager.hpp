50a9b8286f777480b53f4ec2e4607fb866747716
李喆 <240078316@qq.com>
2018-11-18 17:23:49 +0800

    EM

--- driver/engine_manager.hpp
+++ driver/engine_manager.hpp
@@ -6,6 +6,7 @@
 #include <atomic>
 #include <mutex>
 #include <condition_variable>
+#include <deque>
 
 namespace csci5570 {
 
@@ -26,6 +27,11 @@ class EngineManager : public Engine{
     	mailbox_->Start();
     	sender_.reset(new Sender(mailbox_.get()));
     	sender_->Start();
+
+    	// see if it needs to be changed !!!
+		std::vector<uint32_t> sids = id_mapper_->GetServerThreadsForId(node_.id);
+		HeartBeatServerThread_ = new ServerThread(sids.size()-1);
+
     	StartHeartBeat();
 	}
 
@@ -49,7 +55,7 @@ class EngineManager : public Engine{
 	StopAllEngines(){
 		for (int i = 0; i < nodes.size(); i++){
 			if (node_ != nodes_[i]){
-      			engine_group_[i]->StopEverything();
+      			engine_group_[i].get()->StopEverything();
   			}
 		}
 	}
@@ -57,21 +63,58 @@ class EngineManager : public Engine{
 	void HeartBeat(){
 		
 		// call get thread_id with node_id as parameter for communication
-		uint32_t from_node_thread_id = GetHeartBeatThreadForId(node_.id);
+		uint32_t from_node_thread_id = id_mapper->GetHeartBeatThreadForId(node_.id);
+		time_t timestamp = time(NULL);
 
 		for(int i = 0; i < nodes_.size(); i++) {
 
-			uint32_t to_node_thread_id = GetHeartBeatThreadForId(nodes_[i].id);
+			uint32_t to_node_thread_id = id_mapper->GetHeartBeatThreadForId(nodes_[i].id);
 
 			Message msg // the heartbeat message
 		    msg.meta.flag = Flag::kHeartbeat;
 		    msg.meta.sender = from_node_thread_id;
 		    msg.meta.recver = to_node_thread_id;
-		    msg.meta.timestamp = time(NULL);
+		    msg.meta.timestamp = timestamp;
 
 			sender_.get()->GetMessageQueue()->Push(msg);
+			heartbeat_count_[to_node_thread_id].push_back(timestamp);
 		}
 		sender_->Send();
+
+		// add this time stamp to all the nodes's already sent heartbeat_count_
+
+	}
+
+	bool HeartBeatDetection(int threshold = 4){
+		
+		// get heartbeat server thread's messages
+      	HeartBeatServerThread_.get()->GetWorkQueue();
+
+		// check message queue, put it into maps
+		ThreadsafeQueue<Message>* queue;
+		while(queue->Size() > 0){
+			Message msg;
+			queue->WaitAndPop(msg);
+
+			// check maps
+			if(heartbeat_count_.find(msg.sender) == heartbeat_count_.end()){
+				deque<time_t> deq;
+				heartbeat_count_.insert(pair<uint32_t, deque<time_t>>(msg.sender, new deque<time_t>));
+			} else {
+				for (int i = 0; i < heartbeat_count_[msg.sender].size(); i++){
+					if(heartbeat_count_[msg.sender][i] < msg.timestamp){
+						heartbeat_count_.pop_front();
+					}
+				}
+			}
+			// if more than 3, restart that engine.
+		}
+
+		for(std::map<uint32_t, deque<time_t>>::iterator it = heartbeat_count_.begin(); it != heartbeat_count_.end(); ++it){
+			if(it->second.size() >= threshold){
+				// restart this engine...
+			}
+		}
 	}
 
 	// timer in seconds
@@ -81,10 +124,17 @@ class EngineManager : public Engine{
             return false;
         }
         expired_ = false;
+
+        heartbeat_count_.clear();
+        for(int i = 0; i < nodes_.size(); i++){
+        	heartbeat_count_.insert(pair<uint32_t, deque<time_t>>(id_mapper->GetHeartBeatThreadForId(nodes_[i].id), new deque<time_t>));
+        }
+
         std::thread([this, interval](){
             while (!try_to_expire_){
 	            std::this_thread::sleep_for(std::chrono::seconds(interval));
 	            HeartBeat();
+	            HeartBeatDetection();
             }
             // stop timer task
             {
@@ -117,24 +167,16 @@ class EngineManager : public Engine{
         }
 	}
 
-	bool HeartBeatDetection(){
-		// get heartbeat server thread
-
-		// check message queue, put it into maps
-
-		// check maps
-
-		// if more than 3, restart that engine.
-	}
-
  private:
  	// Node node_;
  	// std::vector<Node> nodes_; // the EM will occupy an entire Node! the nodes includes all the nodes.
 	// std::unique_ptr<Mailbox> mailbox_; // inherit from engine
 	// std::unique_ptr<Sender> sender_; // inherit from engine
+	
 	bool isPrimary_ = true; // denote the primary engine manager if yes, or the secondary engine manager if false
-	ThreadsafeQueue<Message> message_queue_; // all the message
-	map<uint32_t, vector<time_t>> message_count_; // thread_id, each engine's heartbeat message
+	map<uint32_t, deque<time_t>> heartbeat_count_; // thread_id, each engine's heartbeat message
+
+	std::unique_ptr<ServerThread> HeartBeatServerThread_;
 
 	std::vector<std::unique_ptr<Engine>> engine_group_;
 
