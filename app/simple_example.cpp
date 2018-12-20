e81ad4e8f80e51f38045e8b2145fbbcd8548146f
maxHH <490949611@qq.com>
2018-12-20 16:11:54 +0800

    solve conflict

--- app/simple_example.cpp
+++ app/simple_example.cpp
@@ -16,8 +16,9 @@
 
 using namespace csci5570;
 
-using Sample = double;
+using Sample = lib::LabeledSample<std::vector<std::pair<int, int>>, int>;
 using DataStore = std::vector<Sample>;
+using Parse = std::function<Sample(boost::string_ref, int)>;
 
 DEFINE_string(config_file, "", "The config file path");
 DEFINE_string(input, "", "The hdfs input url");
@@ -31,35 +32,61 @@ int main(int argc, char** argv) {
   LOG(INFO) << FLAGS_config_file;
 
   Node node{0, "localhost", 12353};
-  Engine engine(node, {node});
 
-  // 1. Start system
+  bool recovery = true;
+
+  Engine engine(node, {node});
   engine.StartEverything();
 
-  // 1.1 Create table
-  const auto kTableId = engine.CreateTable<double>(ModelType::SSP, StorageType::Map);  // table 0
+  uint32_t kTableId = 0;
+  int min_clock = 0;
+
+  if (recovery) {
+    //   engine = Engine(node, {node}, true);
+    min_clock = engine.RecoveryEngine();
+    //   engine.Barrier();
+  } else {
+    // 1.1 Create table
+    kTableId = engine.CreateTable<double>(ModelType::ASP, StorageType::Map);  // table 0
+    engine.Barrier();
+  }
 
   // 1.2 Load data
-  engine.Barrier();
+
+  // double lambda = 0.1;
+  // double alpha = 0.01;
+  // int n_features = 123;
+  // std::string url = "hdfs:///datasets/classification/a9";
+  // DataStore data;
+  // Parse parse(lib::Parser<Sample, DataStore>::parse_libsvm);
+  // lib::AbstractDataLoader<Sample, DataStore>::load<Parse>(url, n_features, parse, &data);
 
   // 2. Start training task
   MLTask task;
   task.SetWorkerAlloc({{0, 3}});  // 3 workers on node 0
   task.SetTables({kTableId});     // Use table 0
-  task.SetLambda([kTableId](const Info& info) {
+  task.SetLambda([kTableId, min_clock](const Info& info) {
     LOG(INFO) << info.DebugString();
 
     KVClientTable<double> table = info.CreateKVClientTable<double>(kTableId);
 
-    for (int i = 0; i < 1e3; ++i) {
-      std::vector<Key> keys{1};
+    for (int i = min_clock; i < 20; ++i) {
+      std::vector<Key> parameter_keys;  // parameters index
+      for (int i = 0; i < 10; ++i)
+        parameter_keys.push_back(i);
+      // std::vector<Key> parameter_values;
+      // for (int i = 0; i < 10; ++i)
+      //   parameter_keys.push_back(i);
 
       std::vector<double> ret;
-      table.Get(keys, &ret);
-      LOG(INFO) << ret[0];
+      table.Get(parameter_keys, &ret);
+      for (int i = 0; i < ret.size(); i++) {
+        std::cout << "parameter is :" << ret[i] << std::endl;
+        ret[i]++;
+      }
 
-      std::vector<double> vals{0.5};
-      table.Add(keys, vals);
+      // std::vector<double> vals{0.5};
+      table.Add(parameter_keys, ret);
 
       table.Clock();
     }
@@ -70,8 +97,6 @@ int main(int argc, char** argv) {
   // 3. Stop
   engine.StopEverything();
 
-
   EngineManager em(node, {node});
-  std::cout << "test\n";
   return 0;
 }
