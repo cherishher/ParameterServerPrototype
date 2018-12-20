861203f4031485d8d9c6add22379784367b31bb5
maxHH <490949611@qq.com>
2018-12-20 21:28:18 +0800

    solve confilict problem

--- lib/abstract_data_loader.hpp
+++ lib/abstract_data_loader.hpp
@@ -9,32 +9,6 @@
 #include "io/line_input_format.hpp"
 
 namespace csci5570 {
-<<<<<<< HEAD
-  namespace lib {
-    
-    // template <typename Sample, template <typename> typename DataStore<Sample>>
-    template <typename Sample, typename DataStore>
-    class AbstractDataLoader {
-    public:
-      /**
-       * Load samples from the url into datastore
-       *
-       * @param url          input file/directory
-       * @param n_features   the number of features in the dataset
-       * @param parse        a parsing function
-       * @param datastore    a container for the samples / external in-memory storage abstraction
-       */
-      template <typename Parse>  // e.g. std::function<Sample(boost::string_ref, int)>
-      static void load(std::string url, int n_features, Parse parse, DataStore* datastore) {
-        // 1. Connect to the data source, e.g. HDFS, via the modules in io
-        // 2. Extract and parse lines
-        // 3. Put samples into datastore
-      }
-      
-    };  // class AbstractDataLoader
-    
-  }  // namespace lib
-=======
 namespace lib {
 
 // template <typename Sample, template <typename> typename DataStore<Sample>>
@@ -109,5 +83,4 @@ class AbstractDataLoader {
 };  // class AbstractDataLoader
 
 }  // namespace lib
->>>>>>> 9bfbd12adef81ed455c95199cf3cb07a98eb01f4
 }  // namespace csci5570
